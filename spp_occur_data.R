b2b084166208ece54c21db3d375e3491e6488ddf
TerrellRoulston <terrellroulston@hotmail.ca>
2024-02-16 13:46:52 -0800

    download

--- spp_occur_data.R
+++ spp_occur_data.R
@@ -1,3 +1,40 @@
 # Top ---------------------------------------------------------------------
-#Data collection for Malus CWR (M. cornistica, M. fusca)
+# Data collection for Malus CWR (M. cornistica, M. fusca)
+# Terrell Roulston
+# Started Feb 16, 2024
 
+library(tidyverse) #data management, grammar
+library(rgbif) #access GBIF data
+
+# Download data from GBIF
+
+# GBIF use info
+user='terrell_roulston'
+pwd='MalusApples#123'
+email='terrellroulston@gmail.com'
+
+# Taxon IDs
+# M coronaria = 3001166
+# M fusca = 3001080
+
+# Coronaria download ------------------------------------------------------
+
+taxonKey <- 3001166
+basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE') # excluded living specimens and material samples (germplasm)
+hasCoordinates <- TRUE # limit to records with coordinates
+years <- seq(1970, 2024, 1) # 1970 to modern day - this is baseline
+
+# Download data
+# Use 'pred()' if there is a single argument, or 'pred_in()' if there are multiple
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred_in("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  pred_in("year", years),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+setwd("./") 
+occu_coronaria <- occ_download_get(down_code[1], overwrite = TRUE)
+
+View(occu_coronaria)
