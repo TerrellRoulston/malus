c1c02e191af2eb1a4b0b352de2e1b77e5fd591b8
TerrellRoulston <terrellroulston@hotmail.ca>
2024-02-16 14:34:46 -0800

    Occurences

--- gbif_occ.R
+++ gbif_occ.R
@@ -39,3 +39,31 @@ occ_coronaria <- occ_download_get(down_code[1], overwrite = TRUE)
 #extract and save as csv
 
 gbif_cor <- read.csv(file = "occ_coronaria.csv") # load csv data
+
+#do not push large datasets to github, add to ignore
+
+
+# Fusca download ----------------------------------------------------------
+
+taxonKey <- 3001080
+basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE') # excluded living specimens and material samples (germplasm)
+hasCoordinates <- TRUE # limit to records with coordinates
+years <- seq(1970, 2024, 1) # 1970 to modern day - this is baseline
+
+# Download data
+# Use 'pred()' if there is a single argument, or 'pred_in()' if there are multiple
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred_in("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  pred_in("year", years),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_fusca <- occ_download_get(down_code[1], overwrite = TRUE)
+#extract and save as csv
+
+gbif_fusca <- read.csv(file = "occ_coronaria.csv") # load csv data
+
+#do not push large datasets to github, add to ignore
