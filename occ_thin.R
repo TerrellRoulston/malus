5efaec6f83cfc71e9f2b474a9c8384a87368b2f8
TerrellRoulston <terrellroulston@hotmail.ca>
2024-04-03 15:59:33 -0700

    removed spurious code

--- occ_thin.R
+++ occ_thin.R
@@ -3,17 +3,15 @@
 # Terrell Roulston
 # Started Feb 20, 2024
 
-library(tidyverse)
-library(dismo)
-library(terra) 
-library(raster)
-library(sp)
+library(tidyverse) # grammar and data management 
+library(terra) # working with spatial data
+library(geodata) # basemaps and climate data
 
 
 # Load cleaned occurrence data ---------------------------------------------
 setwd("../occ_data/")
-occ_cor <- readRDS(file = "occ_cor.Rdata") # n = 518
-occ_fus <- readRDS(file = 'occ_fus.Rdata') # n = 985
+occ_cor <- readRDS(file = "occ_cor.Rdata")
+occ_fus <- readRDS(file = 'occ_fus.Rdata') 
 
 # vectorize occurrence df to coordinates for bellow
 occ_cor <- vect(occ_cor, geom = c('decimalLongitude', 'decimalLatitude'),
@@ -28,15 +26,12 @@ setwd("../wclim_data/")
 wclim <- worldclim_global(var = 'bio', res = 2.5, version = '2.1', path = "../wclim_data/")
 
 # plot a raster to check it downloaded properly
-plot(wclim$wc2.1_2.5m_bio_1)
-
-occ_corALL <- occ_cor # keep for later
-occ_fusALL <- occ_fus # keep for later
+plot(wclim$wc2.1_2.5m_bio_1, main = 'Annual Mean Temperature')
 
 
 # Thin data using sampler -------------------------------------------------
 
-set.seed(1337) # set random generator seed to get reproducable results
+set.seed(1337) # set random generator seed to get reproducible results
 
 # M. coronaria thinning
 occ_cor <- spatSample(occ_cor, size = 1, 
@@ -46,18 +41,8 @@ occ_cor <- spatSample(occ_cor, size = 1,
 occ_fus <- spatSample(occ_fus, size = 1,
                       strata = wclim) #sample one occurrence from each climatic cell
 
-# take a look at the thinned points
 
-# plot(canUS_map, xlim = c(-85, -80), ylim = c(40.75, 41.25))
-# plot(wclim$wc2.1_2.5m_bio_1, legend = F, add = T)
-# plot(canUS_map, add = T)
-# points(occ_corALL, pch = 16,col = "black")
-# points(occ_cor, col = 'blue', pch = 1)
-
-# dev.off()
-
-
-# Save thinned occurence points for further analysis ----------------------
+# Save thinned occurrence points for further analysis ----------------------
 setwd("../occ_data/")
 saveRDS(occ_cor, file = 'occThin_cor.Rdata')
 saveRDS(occ_fus, file = 'occThin_fus.Rdata')
