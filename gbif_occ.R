609f0665a7f85ee797dec2d426b374e210fe6e70
TerrellRoulston <terrellroulston@hotmail.ca>
2024-09-24 11:58:30 -0300

    fixed basis of record typos

--- gbif_occ.R
+++ gbif_occ.R
@@ -7,15 +7,15 @@
 library(tidyverse) # data management, grammar
 library(rgbif) # access GBIF data
 
-getwd()
+  getwd()
 setwd('../malus/')
 
 # Download occurrence data from GBIF
 
 # GBIF user info
-user='REDACTED'
-pwd='REDACTED'
-email='REDACTED'
+user='terrell_roulston'
+pwd='Malus123!'
+email='terrellroulston@gmail.com'
 
 # Taxon IDs
 # M coronaria = 3001166
@@ -24,7 +24,7 @@ email='REDACTED'
 # Coronaria download ------------------------------------------------------
 
 taxonKey <- 3001166
-basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE', 'MATTERIAL_SAMPLES', 'LIVING_SPECIMEN') 
+basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE', 'MATERIAL_SAMPLE', 'LIVING_SPECIMEN') 
 hasCoordinates <- TRUE # limit to records with coordinates
 #years <- seq(1970, 2024, 1) # 1970 to modern day - this is baseline
 # Tyler suggest not limiting records to 1970
@@ -53,10 +53,10 @@ gbif_cor <- read.csv(file = "occ_coronaria.csv") # load named csv data
 # fusca download ----------------------------------------------------------
 
 taxonKey <- 3001080
-basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE', 'MATTERIAL_SAMPLES', 'LIVING_SPECIMEN')
+basisOfRecord <- c('PRESERVED_SPECIMEN', 'HUMAN_OBSERVATION', 'OCCURRENCE', 'MATERIAL_SAMPLE', 'LIVING_SPECIMEN')
 hasCoordinates <- TRUE # limit to records with coordinates
-#years <- seq(1970, 2024, 1) # 1970 to modern day - this is baseline
-#see comments above about years of occurrence
+# years <- seq(1970, 2024, 1) # 1970 to modern day - this is baseline
+# see comments above about years of occurrence
 
 # Download data
 # Use 'pred()' if there is a single argument, or 'pred_in()' if there are multiple
