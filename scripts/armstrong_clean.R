f4565ce41f84755439e5e03492d28d5b6b2d7a5a
TerrellRoulston <terrellroulston@hotmail.ca>
2025-02-27 15:14:54 -0400

    Script to explore and clean m fusca data from CG Armstrong

---
+++ scripts/armstrong_clean.R
@@ -0,0 +1,71 @@
+# Top ---------------------------------------------------------------------
+# cleaning Malus fusca records from Chelsey Geralda Armstrong
+# Terrell Roulston
+# Started Feb 26, 2025
+
+#Issues in the coordinates for records converted from UTM measurments incorreclty need to be adjusted
+#this data is corrected in the occurrence data folder (malus_fusca_armstrong.csv)
+
+library(tidyverse)
+library(geodata)
+library(terra)
+library(oce)
+
+
+# Load basemaps -----------------------------------------------------------
+
+us_map <- gadm(country = 'USA', level = 1, resolution = 2,
+               path = "./maps/base_maps") #USA basemap w. States
+
+ca_map <- gadm(country = 'CA', level = 1, resolution = 2,
+               path = './maps/base_maps') #Canada basemap w. Provinces
+
+mex_map <-gadm(country = 'MX', level = 1, resolution = 2,
+               path = './maps/base_maps') # Mexico basemap w. States
+
+canUSMex_map <- rbind(us_map, ca_map, mex_map) # Combine Mexico, US and Canada vector map
+
+
+
+# Load Malus fusca data from CG Armstrong ---------------------------------
+armstrong <- readxl::read_xlsx("C:/Users/terre/Documents/Acadia/Malus Project/ex situ data/Core MF geodata.xlsx") 
+armstrong <- armstrong  %>% filter(Latitude != 'NA') # drop one record that does have any coordinates
+
+# There apprears to be an issues with some of the coordiates, I suspect that the UTMs were converted incorrectly.
+plot(canUSMex_map, xlim = c(-180, -100), ylim = c(30, 70))
+points(x=armstrong$Longitude, y=armstrong$Latitude, col = 'red')
+
+# Seperate UTM into easting and northing values
+armstrong <- armstrong %>% separate(UTM, into = c("Easting", "Northing"), sep = ", ", convert = TRUE, remove = FALSE)
+
+# filter and convert occ form UTM.ZONE 9
+armstrong_utm_9 <- armstrong %>% filter(UTM.ZONE == 9)
+armstrong_latLon_9 <- as.data.frame(utm2lonlat(easting = armstrong_utm_9$Easting, northing = armstrong_utm_9$Northing, zone = 9, hemisphere = 'N')) %>% rename(newLongitude = longitude, newLatitude = latitude)
+#Join the new lat/lon values with the utm df to rejoin with main df
+armstrong_utm_9 <- cbind(armstrong_utm_9, armstrong_latLon_9)
+
+# filter and convert occ form UTM.ZONE 10
+armstrong_utm_10 <- armstrong %>% filter(UTM.ZONE == 10)
+armstrong_latLon_10 <- as.data.frame(utm2lonlat(easting = armstrong_utm_10$Easting, northing = armstrong_utm_10$Northing, zone = 10, hemisphere = 'N')) %>% rename(newLongitude = longitude, newLatitude = latitude)
+#Join the new lat/lon values with the utm df to rejoin with main df
+armstrong_utm_10 <- cbind(armstrong_utm_10, armstrong_latLon_10)
+
+#Now join the utm df with the main df
+
+armstrong <- left_join(armstrong, armstrong_utm_9)
+armstrong <- left_join(armstrong, armstrong_utm_10)
+
+#Now replot to check if fixed
+
+# There apprears to be an issues with some of the coordiates, I suspect that the UTMs were converted incorrectly.
+plot(canUSMex_map, xlim = c(-160, -110), ylim = c(40, 70), main = 'Armstong M. fusca data')
+points(x=armstrong$Longitude, y=armstrong$Latitude, col = 'red', pch = 16) #old
+points(x=armstrong_utm_9$newLongitude, y=armstrong_utm_9$newLatitude, col = 'blue',pch = 16) #new
+points(x=armstrong_utm_10$newLongitude, y=armstrong_utm_10$newLatitude, col = 'blue', pch = 16) #new
+
+legend(x= -150,
+       y = 50,
+       title = 'Recalculated UTM Occurrences',
+       legend = c('Originial Points', 'Adjusted Points'),
+       fill = c('red', 'blue'))
+
