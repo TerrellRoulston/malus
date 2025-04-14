9b35cd0ca341afcd072b046659c4ae6b8dbdafb8
TerrellRoulston <terrellroulston@hotmail.ca>
2025-04-14 16:01:48 -0300

    Finish cleaning and plot Armstrong data

--- scripts/armstrong_clean.R
+++ scripts/armstrong_clean.R
@@ -29,7 +29,7 @@ canUSMex_map <- rbind(us_map, ca_map, mex_map) # Combine Mexico, US and Canada v
 
 # Load Malus fusca data from CG Armstrong ---------------------------------
 armstrong <- readxl::read_xlsx("C:/Users/terre/Documents/Acadia/Malus Project/ex situ data/Core MF geodata.xlsx") 
-armstrong <- armstrong  %>% filter(Latitude != 'NA') # drop one record that does have any coordinates
+armstrong <- armstrong  %>% filter(Easting != 'NA') # drop one record that does have any coordinates
 
 # There apprears to be an issues with some of the coordiates, I suspect that the UTMs were converted incorrectly.
 plot(canUSMex_map, xlim = c(-180, -100), ylim = c(30, 70))
@@ -37,15 +37,17 @@ points(x=armstrong$Longitude, y=armstrong$Latitude, col = 'red')
 
 # Seperate UTM into easting and northing values
 armstrong <- armstrong %>% separate(UTM, into = c("Easting", "Northing"), sep = ", ", convert = TRUE, remove = FALSE)
-
+armstrong$Easting <- as.numeric(armstrong$Easting)
+armstrong$Northing <- as.numeric(armstrong$Northing)
 # filter and convert occ form UTM.ZONE 9
-armstrong_utm_9 <- armstrong %>% filter(UTM.ZONE == 9)
-armstrong_latLon_9 <- as.data.frame(utm2lonlat(easting = armstrong_utm_9$Easting, northing = armstrong_utm_9$Northing, zone = 9, hemisphere = 'N')) %>% rename(newLongitude = longitude, newLatitude = latitude)
+
+armstrong_utm_9 <- armstrong %>% filter(`UTM ZONE` == 9)
+armstrong_latLon_9 <- as.data.frame(utm2lonlat(easting = armstrong_utm_9$Easting, northing = armstrong_utm_9$Northing, zone = 9, hemisphere = 'N', km = F)) %>% rename(newLongitude = longitude, newLatitude = latitude)
 #Join the new lat/lon values with the utm df to rejoin with main df
 armstrong_utm_9 <- cbind(armstrong_utm_9, armstrong_latLon_9)
 
 # filter and convert occ form UTM.ZONE 10
-armstrong_utm_10 <- armstrong %>% filter(UTM.ZONE == 10)
+armstrong_utm_10 <- armstrong %>% filter(`UTM ZONE` == 10)
 armstrong_latLon_10 <- as.data.frame(utm2lonlat(easting = armstrong_utm_10$Easting, northing = armstrong_utm_10$Northing, zone = 10, hemisphere = 'N')) %>% rename(newLongitude = longitude, newLatitude = latitude)
 #Join the new lat/lon values with the utm df to rejoin with main df
 armstrong_utm_10 <- cbind(armstrong_utm_10, armstrong_latLon_10)
@@ -68,4 +70,23 @@ legend(x= -150,
        title = 'Recalculated UTM Occurrences',
        legend = c('Originial Points', 'Adjusted Points'),
        fill = c('red', 'blue'))
+
+
+
+# Overlay GBIF and Armstrong data -----------------------------------------
+occ_fus <- readRDS(file = './occ_data/fus/occ_fus.Rdata')
+occ_armstrong <- read.csv(file = "./occ_data/fus/malus_fusca_armstrong.csv") #Note that this is only Armstrong data (not a combination of GBIF data)
+
+
+occ_fus <- vect(occ_fus, geom = c('decimalLongitude', 'decimalLatitude'),
+                crs = "+proj=longlat +datum=WGS84")
+occ_armstrong <- vect(occ_armstrong, geom = c('Longitude', 'Latitude'),
+                crs = "+proj=longlat +datum=WGS84")
+
+
+plot(canUSMex_map, xlim = c(-160, -110), ylim = c(40, 70), main = 'Armstong M. fusca data')
+points(x=occ_fus, col = 'red', pch = 16) #old
+points(occ_armstrong, col = 'blue',pch = 16) #new
+
+
 
