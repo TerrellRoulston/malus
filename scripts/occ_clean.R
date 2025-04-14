e8ab1f0057483bae27cbfda57b50c813ed668bb6
TerrellRoulston <terrellroulston@hotmail.ca>
2025-04-14 15:58:05 -0300

    cleaning occurrence data from M angustifolia and M ioensis GBIF data. Some other changes to updating directories

--- scripts/occ_clean.R
+++ scripts/occ_clean.R
@@ -1,5 +1,5 @@
 # Top ---------------------------------------------------------------------
-# cleaning occurrence data of M. coronaria and M. fusca
+# cleaning occurrence data of M. coronaria, M. fusca, M. ioensis, and M. angustifolia
 # Terrell Roulston
 # Started Feb 16, 2024
 
@@ -8,19 +8,16 @@ library(CoordinateCleaner) #helpful functions to clean data
 library(terra) #working with vector/raster data
 library(geodata) #download basemaps
 library(scales) #alpha adjust colours
-
-# set wd
-getwd()
-setwd("../occ_data/") 
+library(oce)
 
 
 # load occurrence csv files ------------------------------------------------
-gbif_cor <- read.csv(file = "./occ_data/occ_coronaria.csv") # load coronaria data
-gbif_fusca <- read.csv(file = "./occ_data/occ_fusca.csv") # load fusca data
+gbif_cor <- read.csv(file = "./occ_data/cor/occ_coronaria.csv") # load coronaria data
+gbif_fus <- read.csv(file = "./occ_data/fus/occ_fusca.csv") # load fusca data
+gbif_ion <- read.csv(file = "./occ_data/ion/occ_ioensis.csv") # load ioensis data
+gbif_ang <- read.csv(file = "./occ_data/ang/occ_angustifolia.csv") # load angustifolia data
 
-
-# filter occurrence data in df ---------------------------------------------
-
+# Clean Coronaria ---------------------------------------------------------
 # filter M. corornia data
 occ_cor <- gbif_cor %>% 
   filter(countryCode %in% c('US', 'CA')) %>% #limit to CA and US
@@ -34,21 +31,22 @@ occ_cor <- gbif_cor %>%
   filter(!(decimalLatitude < 35 & decimalLongitude < -86)) %>% # remove records from Texas, Oklahoma, Louisiana
   filter(!(decimalLatitude > 45)) %>% # remove record from New Brunswick
   filter(!(decimalLongitude < -98)) %>% # remove iNat record from Kansas, northern Kansas record verified by taxonomist
+  filter(!(catalogNumber == 174554608)) %>% # remove inaccurate record 	https://www.inaturalist.org/observations/174554608
+  filter(!(catalogNumber == 181065977)) %>% # remove inaccurate record 	https://www.inaturalist.org/observations/181065977
   dplyr::select(species, countryCode, decimalLatitude, 
          decimalLongitude, coordinateUncertaintyInMeters, year, basisOfRecord
          )
 
+saveRDS(occ_cor, file = "./occ_data/cor/occ_cor_gbif.Rdata") # Note that this copy of occurrence data is used for the sythesis paper in figure 3.
 
 # Note it is helpful to plot the occurrences bellow, and then add more conditions to clean inaccurate points
 # Pay special attention to points at the edge of the range of occurrences, as these are most likely to be suspicious
 # as well as influence the model in strange ways, in comparison to inaccurate points well within the other occurrences
 
-# Save M. coronaria occurrence dataframe ----------------------------------
-saveRDS(occ_cor, file = "occ_cor.Rdata") 
 
-
+# Import Brian Husband's data ---------------------------------------------
 ## Brian's verified Ontario records
-husband <- read.table("occ_data/malus_coronaria_husband.csv",
+husband <- read.table("occ_data/cor/malus_coronaria_husband.csv",
                       header = TRUE, sep = "\t")
 
 ## remove records that don't have valid coordinates
@@ -63,21 +61,36 @@ husband$lon <- sapply(husband$tmp, FUN = function(x) (-1 * as.numeric(x[5])
 
 husband$tmp <- NULL
 
-# compare pre/post 1970 occurrence data
-# if no points between pre/post are drastically different then keep all data
-# consider removing if different from post-1970
+## Swap in Brian Husband's records for Ontario:
 
-occ_cor_pre <- occ_cor %>% # pre 1970
-  filter(year < 1970)
+#occ_husband <- vect(occ_cor, geom = c("decimalLongitude", "decimalLatitude"))
+#ont <- ca_map[ca_map$NAME_1 == "Ontario"]
 
-occ_cor_post <- occ_cor %>% # post 1970
-  filter(year >= 1970)
+#occ_husband <- occ_husband[!relate(occ_husband, ont, "intersects")]
 
-occ_cor_nd <- occ_cor %>% 
-  filter(year %in% NA)
+#husband <- vect(husband, geom = c("lon", "lat"))
 
+#occ_husband <- rbind(occ_husband, husband)
+
+#saveRDS(occ_husband, file = "occ_data/cor/occ_husband.Rdata") # this has US reocrds from GBIF and CA recrods from Brian
+
+# Keep all GBIF records but add Brian's data
+
+husband_coords <- husband %>% select(Accession, lat, lon) %>% mutate(source = 'Husband') %>% 
+                              mutate(species = 'Malus coronaria') %>% 
+                              rename(decimalLatitude = lat, decimalLongitude = lon) #rename lat lon to match for inner join
+                
+occ_cor <- occ_cor %>%  mutate(source = 'GBIF') # add source for tracking and mapping purposes
+
+occ_cor <- occ_cor %>% full_join(husband_coords, by = c("decimalLatitude", "decimalLongitude", "source", "species"))
+
+# Save M. coronaria occurrence dataframe
+saveRDS(occ_cor, file = "./occ_data/cor/occ_cor.Rdata") # Note this copy of occurrence data will be used in downstream SDM work
+
+
+# Clean Fusca -------------------------------------------------------------
 # filter M. fusca data in df
-occ_fus <- gbif_fusca %>% 
+occ_fus <- gbif_fus %>% 
   filter(countryCode %in% c('US', 'CA')) %>% #limit to CA and US
   filter(!is.na(decimalLongitude)) %>% # remove records w/o coords
   filter(coordinateUncertaintyInMeters < 30000 | is.na(coordinateUncertaintyInMeters)) %>% 
@@ -88,118 +101,74 @@ occ_fus <- gbif_fusca %>%
   filter(!(decimalLongitude > -113)) %>%  # remove record from Idaho herbarium 
   filter(!(decimalLatitude > 64)) %>% # remove record from Interior Alaska
   filter(!(stateProvince == 	'Idaho')) %>% # remove phishy record from Idaho
+  filter(!(gbifID == '4908101518')) %>% # questionble outliying Inaturalist observation.
+  filter(!(decimalLatitude == 37.27013)) %>% # remove bad record in mountains inland California
+  filter(!(decimalLatitude < 37)) %>% # remove unlikely records south of San Jose
   dplyr::select(species, countryCode, decimalLatitude, 
          decimalLongitude, coordinateUncertaintyInMeters, year, basisOfRecord
   )
 
+saveRDS(occ_fus, file = "./occ_data/fus/occ_fus_gbif.Rdata") # Note that this copy of occurrence data is used for the sythesis paper in figure 3.
 
-# Save M. fusca occurrence dataframe --------------------------------------
-saveRDS(occ_fus, file = "occ_fus.Rdata")
+# load cleaned fusca data from CG Amrstrong
+occ_armstrong <- read.csv(file = "./occ_data/fus/malus_fusca_armstrong.csv") %>% #Note that this is only Armstrong data (not a combination of GBIF data)
+                  mutate(species = 'Malus fusca') %>% # add species
+                  mutate(source = 'Armstrong') %>% # add source
+                  select(Latitude, Longitude, source, species) %>% 
+                  rename(decimalLatitude = Latitude, decimalLongitude = Longitude)
+                  
+occ_fus <- occ_fus %>% mutate(source = 'GBIF')
 
-# compare pre/post 1970 occurrences
-occ_fus_pre <- occ_fus %>% 
-  filter(year < 1970)
+occ_fus <- occ_fus %>% full_join(occ_armstrong, by = c("decimalLatitude", "decimalLongitude", "source", "species"))
 
-occ_fus_post <- occ_fus %>% 
-  filter(year >= 1970)
+saveRDS(occ_fus, file = "./occ_data/fus/occ_fus.Rdata") # Note this copy of occurrence data will be used in downstream SDM work
 
-occ_fus_nd <- occ_fus %>% 
-  filter(year %in% NA)
+#load data from Wickham and Obrits and Fitsp
+occ_wick_orb_fit <- read.csv(file = "./occ_data/fus/malus_fusca_wickham_orbits_titzpatrick.csv") %>% 
+  select(latitude, longitude, source, species) %>% 
+  rename(decimalLatitude = latitude, decimalLongitude = longitude)
 
-# map occurrence points (take a peak) -------------------------------------
+occ_fus <- occ_fus %>% full_join(occ_wick_orb_fit, by = c("decimalLatitude", "decimalLongitude", "source", "species"))
 
-# download/load maps
-us_map <- gadm(country = 'USA', level = 1, resolution = 2,
-               path = "../occ_data/base_maps") #USA basemap w. States
+saveRDS(occ_fus, file = "./occ_data/fus/occ_fus.Rdata") # Note this copy of occurrence data will be used in downstream SDM work
 
-ca_map <- gadm(country = 'CA', level = 1, resolution = 2,
-               path = '../occ_data/base_maps') #Canada basemap w. Provinces
+# Clean Ioensis -----------------------------------------------------------
+occ_ion <- gbif_ion %>% 
+  filter(countryCode %in% c('US')) %>% #limit to US
+  filter(!is.na(decimalLongitude)) %>% # remove records w/o coords
+  filter(coordinateUncertaintyInMeters < 30000 | is.na(coordinateUncertaintyInMeters)) %>% 
+  cc_cen(buffer = 2000) %>% # remove records within 2km of country centroids
+  cc_inst(buffer = 2000) %>% # remove records within 2km of herbariums, bot cardens 
+  cc_sea() %>% 
+  distinct(decimalLatitude, decimalLongitude, speciesKey, datasetKey, .keep_all = T) %>% 
+  filter(!(decimalLongitude < -99)) %>%  # remove records from west of Kansas/Texas occ
+  filter(!(gbifID %in% c('1899381516', '5067955163'))) %>% #remove questionable GBIF records
+  filter(!(decimalLongitude > -84)) %>%  # remove sporadic east coast records. likely not ioensis based on classical distribtion
+  dplyr::select(species, countryCode, decimalLatitude, 
+                decimalLongitude, coordinateUncertaintyInMeters, year, basisOfRecord
+  )
 
-canUS_map <- rbind(us_map, ca_map) #combine US and Canada vector map
+occ_ion <- occ_ion %>% mutate(source = 'GBIF')
 
-# plot basemap
-plot(canUS_map, xlim = c(-180, -50))
-
-# plot Malus coronia occurrences
-points(occ_cor$decimalLongitude, occ_cor$decimalLatitude, pch = 16,
-       col = alpha("red", 0.2))
-
-# plot Malus fusca occurrences
-points(occ_fus$decimalLongitude, occ_fus$decimalLatitude, pch = 16,
-       col = alpha('blue', 0.2))
-
-# add legend
-legend(x= -170,
-       y = 40,
-       legend = c('M. coronaria', 'M. fusca'),
-       fill = c('red', 'blue'))
-
-# clear graphics plot
-dev.off()
-
-
-# Zoom into each species --------------------------------------------------
-
-# M coronaria
-plot(canUS_map, xlim = c(-100, -60), ylim = c(25, 50))
-# plot Malus coronia occurrences
-# pre-1970
-points(occ_cor_pre$decimalLongitude, occ_cor_pre$decimalLatitude, pch = 16,
-       col = alpha("red", 0.2))
-# post-1970
-points(occ_cor_post$decimalLongitude, occ_cor_post$decimalLatitude, pch = 16,
-       col = alpha("blue", 0.2))
-# No date
-points(occ_cor_nd$decimalLongitude, occ_cor_nd$decimalLatitude, pch = 16,
-       col = alpha('black', 0.2))
-
-legend(x= -75,
-       y = 33,
-       title = 'M. coronaria',
-       legend = c('Pre-1970 (n=422)', 'Post-1970 (n=558)', 'No Date (n=52)'),
-       fill = c('red', 'blue', 'black'))
-
-dev.off() # close graphics plot window
-
-# M fusca
-plot(canUS_map, xlim = c(-170, -110), ylim = c(30, 65))
-# plot Malus fusca occurrences
-# post-1970
-points(occ_fus_post$decimalLongitude, occ_fus_post$decimalLatitude, pch = 16,
-       col = alpha("blue", 0.2))
-# pre-1970
-points(occ_fus_pre$decimalLongitude, occ_fus_pre$decimalLatitude, pch = 16,
-       col = alpha("red", 0.2))
-# No date
-points(occ_fus_nd$decimalLongitude, occ_fus_nd$decimalLatitude, pch = 16,
-       col = alpha('black', 0.2))
+saveRDS(occ_ion, file = "./occ_data/ion/occ_ion.Rdata")
 
 
-legend(x= -165,
-       y = 40,
-       title = 'M. fusca',
-       legend = c('Pre-1970 (n=191)', 'Post-1970 (n=1230)', 'No Date (n=45)'),
-       fill = c('red', 'blue', 'black'))
+# Clean Angustifolia ------------------------------------------------------
+occ_ang <- gbif_ang %>% 
+  filter(countryCode %in% c('US')) %>% #limit to US
+  filter(!is.na(decimalLongitude)) %>% # remove records w/o coords
+  filter(coordinateUncertaintyInMeters < 30000 | is.na(coordinateUncertaintyInMeters)) %>% 
+  cc_cen(buffer = 2000) %>% # remove records within 2km of country centroids
+  cc_inst(buffer = 2000) %>% # remove records within 2km of herbariums, bot cardens 
+  cc_sea() %>% 
+  distinct(decimalLatitude, decimalLongitude, speciesKey, datasetKey, .keep_all = T) %>% 
+  filter(decimalLatitude <= 41.99575 | decimalLongitude <= -72.60754) %>% #filter occ northeast of Connecticut
+  filter(!(gbifID %in% c('3865090173', '4919535276', '1302641832', '3091187987', '4855490969', '4854957347'))) %>%  #remove a record from greenland (???) and bad inat obs
+  dplyr::select(species, countryCode, decimalLatitude, 
+                decimalLongitude, coordinateUncertaintyInMeters, year, basisOfRecord
+  )
+occ_ang <- occ_ang %>% mutate(source = 'GBIF')
 
-dev.off()
-
-# Zoom into PNW
-plot(canUS_map, xlim = c(-140, -110), ylim = c(43, 55))
-# plot Malus fusca occurrences
-points(occ_fus$decimalLongitude, occ_fus$decimalLatitude, pch = 16,
-       col = alpha("blue", 0.2))
+saveRDS(occ_ang,  file = "./occ_data/ang/occ_ang.Rdata")
 
 
-## Swap in Brian Husband's records for Ontario:
-
-occ_husband <- vect(occ_cor, geom = c("decimalLongitude",
-                                      "decimalLatitude"))
-ont <- ca_map[ca_map$NAME_1 == "Ontario"]
-
-occ_husband <- occ_husband[!relate(occ_husband, ont, "intersects")]
-
-husband <- vect(husband, geom = c("lon", "lat"))
-
-occ_husband <- rbind(occ_husband, husband)
-
-saveRDS(occ_husband, file = "occ_data/occ_husband.Rdata")
