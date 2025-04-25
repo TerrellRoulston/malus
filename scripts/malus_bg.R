e7b4cae5f144add3121676fc04853c47d81c21ab
plantarum <tyler@plantarum.ca>
2025-04-25 16:27:43 -0400

    commented out code not used in the pipeline
    Plotting, loading and saving objects; not needed for the final pipeline.

--- scripts/malus_bg.R
+++ scripts/malus_bg.R
@@ -39,8 +39,8 @@ eco_cor_code <- eco_cor_code[eco_cor_code != '0.0']  #remove the 'water' '0.0' e
 
 ecoNA_cor <- terra::subset(ecoNA, ecoNA$NA_L2CODE %in% eco_cor_code) # subset eco region spat vector by the codes
 
-plot(ecoNA_cor) # plot the subseted M. coronaria eco regions
-points(occThin_cor, pch = 3, col = 'red') # plot M. coronaria points
+## plot(ecoNA_cor) # plot the subseted M. coronaria eco regions
+## points(occThin_cor, pch = 3, col = 'red') # plot M. coronaria points
 
 # M. fusca eco regions ----------------------------------------------------
 # # extract eco region polygon that contain M. fusca occurrence points
@@ -57,8 +57,8 @@ eco_fus_code <- eco_fus_code[eco_fus_code != '0.0'] # remove NA value
 
 ecoNA_fus <- subset(ecoNA, ecoNA$NA_L2CODE %in% eco_fus_code)
 
-plot(ecoNA_fus)
-points(occThin_fus, pch = 3, col = 'red') # plot M. coronaria points
+## plot(ecoNA_fus)
+## points(occThin_fus, pch = 3, col = 'red') # plot M. coronaria points
 
 
 # M. ionesis eco regions --------------------------------------------------
@@ -76,8 +76,8 @@ eco_ion_code <- eco_ion_code[eco_ion_code != '0.0'] # remove NA value
 
 ecoNA_ion <- subset(ecoNA, ecoNA$NA_L2CODE %in% eco_ion_code)
 
-plot(ecoNA_ion)
-points(occThin_ion, pch = 3, col = 'red') 
+## plot(ecoNA_ion)
+## points(occThin_ion, pch = 3, col = 'red') 
 
 
 # M. angustifolia eco regions ---------------------------------------------
@@ -95,8 +95,8 @@ eco_ang_code <- eco_ang_code[eco_ang_code != '0.0'] # remove NA value
 
 ecoNA_ang <- subset(ecoNA, ecoNA$NA_L2CODE %in% eco_ang_code)
 
-plot(ecoNA_ang)
-points(occThin_ang, pch = 3, col = 'red') 
+## plot(ecoNA_ang)
+## points(occThin_ang, pch = 3, col = 'red') 
 
 
 # Sect. Chloromeles eco region --------------------------------------------
@@ -114,15 +114,17 @@ eco_chl_code <- eco_chl_code[eco_chl_code != '0.0'] # remove NA value
 
 ecoNA_chl <- subset(ecoNA, ecoNA$NA_L2CODE %in% eco_chl_code)
 
-plot(ecoNA_chl)
-points(occThin_chl, pch = 3, col = 'red') 
+## plot(ecoNA_chl)
+## points(occThin_chl, pch = 3, col = 'red') 
 
+
+## NB: could save this as shapefiles instead!
 # SAVE
-saveRDS(ecoNA_cor, file = './maps/eco_regions/ecoNA_cor.Rdata')
-saveRDS(ecoNA_fus, file = './maps/eco_regions/ecoNA_fus.Rdata')
-saveRDS(ecoNA_ion, file = './maps/eco_regions/ecoNA_ion.Rdata')
-saveRDS(ecoNA_ang, file = './maps/eco_regions/ecoNA_ang.Rdata')
-saveRDS(ecoNA_chl, file = './maps/eco_regions/ecoNA_chl.Rdata')
+## saveRDS(ecoNA_cor, file = './maps/eco_regions/ecoNA_cor.Rdata')
+## saveRDS(ecoNA_fus, file = './maps/eco_regions/ecoNA_fus.Rdata')
+## saveRDS(ecoNA_ion, file = './maps/eco_regions/ecoNA_ion.Rdata')
+## saveRDS(ecoNA_ang, file = './maps/eco_regions/ecoNA_ang.Rdata')
+## saveRDS(ecoNA_chl, file = './maps/eco_regions/ecoNA_chl.Rdata')
 
 # Crop WorldClim to Ecoregions and Create Background ----------------------
 
@@ -135,11 +137,11 @@ wclim_chl <- terra::crop(wclim, ecoNA_chl, mask = T)
 
 # Save cropped wclim data for downsteam SDM workflow
 
-saveRDS(wclim_cor, file = './wclim_data/wclim_cor.Rdata')
-saveRDS(wclim_fus, file = './wclim_data/wclim_fus.Rdata')
-saveRDS(wclim_ion, file = './wclim_data/wclim_ion.Rdata')
-saveRDS(wclim_ang, file = './wclim_data/wclim_ang.Rdata')
-saveRDS(wclim_chl, file = './wclim_data/wclim_chl.Rdata')
+## saveRDS(wclim_cor, file = './wclim_data/wclim_cor.Rdata')
+## saveRDS(wclim_fus, file = './wclim_data/wclim_fus.Rdata')
+## saveRDS(wclim_ion, file = './wclim_data/wclim_ion.Rdata')
+## saveRDS(wclim_ang, file = './wclim_data/wclim_ang.Rdata')
+## saveRDS(wclim_chl, file = './wclim_data/wclim_chl.Rdata')
 
 
 # Sample background points ------------------------------------------------
@@ -155,81 +157,76 @@ set.seed(1337) # set a seed to ensure reproducible results
 
 # Note upped bg points from 5000 to 20000
 cor_bg_vec <- spatSample(wclim_cor, 20000, 'random', na.rm = T, as.points = T) #ignore NA values
-plot(wclim_cor[[1]])
-points(cor_bg_vec, cex = 0.01)
+## plot(wclim_cor[[1]])
+## points(cor_bg_vec, cex = 0.01)
 
-expanse(wclim_cor[[1]], unit = 'km') # total area of raster in km^2
+## expanse(wclim_cor[[1]], unit = 'km') # total area of raster in km^2
 # 5683684 km^2
-20000/5683684
+##20000/5683684
 # 0.00035 samples/km
 
 # M. fusca background
 # SpatVector
 fus_bg_vec <- spatSample(wclim_fus, 20000, 'random', na.rm = T, as.points = T) #ignore NA values
-plot(wclim_fus[[1]])
-points(fus_bg_vec, cex = 0.01)
+## plot(wclim_fus[[1]])
+## points(fus_bg_vec, cex = 0.01)
 
-expanse(wclim_fus[[1]], unit = 'km') # total area of raster in km^2
+## expanse(wclim_fus[[1]], unit = 'km') # total area of raster in km^2
 # 4659175 km^2
 # 5000/4659175 = 0.00107 samples/km
 
 # M. ionesis background
 # SpatVector
 ion_bg_vec <- spatSample(wclim_ion, 20000, 'random', na.rm = T, as.points = T) #ignore NA values
-plot(wclim_ion[[1]])
-points(ion_bg_vec, cex = 0.01)
+## plot(wclim_ion[[1]])
+## points(ion_bg_vec, cex = 0.01)
 
-expanse(wclim_ion[[1]], unit = 'km') # total area of raster in km^2
+## expanse(wclim_ion[[1]], unit = 'km') # total area of raster in km^2
 
 # M. angustifolia background
 # SpatVector
 ang_bg_vec <- spatSample(wclim_ang, 20000, 'random', na.rm = T, as.points = T) #ignore NA values
-plot(wclim_ang[[1]])
-points(ang_bg_vec, cex = 0.01)
+## plot(wclim_ang[[1]])
+## points(ang_bg_vec, cex = 0.01)
 
-expanse(wclim_ang[[1]], unit = 'km') # total area of raster in km^2
+## expanse(wclim_ang[[1]], unit = 'km') # total area of raster in km^2
 
 
 # Sect. Chloromeles background
 # SpatVector
 chl_bg_vec <- spatSample(wclim_chl, 20000, 'random', na.rm = T, as.points = T) #ignore NA values
-plot(wclim_chl[[1]])
-points(chl_bg_vec, cex = 0.01)
+## plot(wclim_chl[[1]])
+## points(chl_bg_vec, cex = 0.01)
 
-expanse(wclim_chl[[1]], unit = 'km') # total area of raster in km^2
+## expanse(wclim_chl[[1]], unit = 'km') # total area of raster in km^2
 
 
-# Save background SpatVectors
-saveRDS(cor_bg_vec, file = './occ_data/cor/cor_bg_vec.Rdata')
-saveRDS(fus_bg_vec, file = './occ_data/fus/fus_bg_vec.Rdata')
-saveRDS(ion_bg_vec, file = './occ_data/ion/ion_bg_vec.Rdata')
-saveRDS(ang_bg_vec, file = './occ_data/ang/ang_bg_vec.Rdata')
-saveRDS(chl_bg_vec, file = './occ_data/chl/chl_bg_vec.Rdata')
+## # Save background SpatVectors
+## saveRDS(cor_bg_vec, file = './occ_data/cor/cor_bg_vec.Rdata')
+## saveRDS(fus_bg_vec, file = './occ_data/fus/fus_bg_vec.Rdata')
+## saveRDS(ion_bg_vec, file = './occ_data/ion/ion_bg_vec.Rdata')
+## saveRDS(ang_bg_vec, file = './occ_data/ang/ang_bg_vec.Rdata')
+## saveRDS(chl_bg_vec, file = './occ_data/chl/chl_bg_vec.Rdata')
 
-# Load background SpatVectors
+## # Load background SpatVectors
 
-cor_bg_vec <- readRDS(file = './occ_data/cor/cor_bg_vec.Rdata')
-fus_bg_vec <- readRDS(file = './occ_data/fus/fus_bg_vec.Rdata')
-ion_bg_vec <- readRDS(file = './occ_data/ion/ion_bg_vec.Rdata')
-ang_bg_vec <- readRDS(file = './occ_data/ang/ang_bg_vec.Rdata')
-chl_bg_vec <- readRDS(file = './occ_data/chl/chl_bg_vec.Rdata')
+## cor_bg_vec <- readRDS(file = './occ_data/cor/cor_bg_vec.Rdata')
+## fus_bg_vec <- readRDS(file = './occ_data/fus/fus_bg_vec.Rdata')
+## ion_bg_vec <- readRDS(file = './occ_data/ion/ion_bg_vec.Rdata')
+## ang_bg_vec <- readRDS(file = './occ_data/ang/ang_bg_vec.Rdata')
+## chl_bg_vec <- readRDS(file = './occ_data/chl/chl_bg_vec.Rdata')
 
 # Extracting presence-background raster values ----------------------------
 
-cor_predvals <- extract(wclim_cor, occThin_cor) # M. coronaria
-cor_predvals <- cor_predvals[-1] # drop ID column
+cor_predvals <- extract(wclim_cor, occThin_cor, ID = FALSE) # M. coronaria
 
-fus_predvals <- extract(wclim_fus, occThin_fus) # M. fusca
-fus_predvals <- fus_predvals[-1] # drop ID column
+fus_predvals <- extract(wclim_fus, occThin_fus, ID = FALSE) # M. fusca
 
-ion_predvals <- extract(wclim_ion, occThin_ion) # M. fusca
-ion_predvals <- ion_predvals[-1] # drop ID column
+ion_predvals <- extract(wclim_ion, occThin_ion, ID = FALSE) # M. fusca
 
-ang_predvals <- extract(wclim_ang, occThin_ang) # M. fusca
-ang_predvals <- ang_predvals[-1] # drop ID column
+ang_predvals <- extract(wclim_ang, occThin_ang, ID = FALSE) # M. fusca
 
-chl_predvals <- extract(wclim_chl, occThin_chl) # M. fusca
-chl_predvals <- chl_predvals[-1] # drop ID column
+chl_predvals <- extract(wclim_chl, occThin_chl, ID = FALSE) # M. fusca
 
 cor_bgvals <- values(cor_bg_vec) # Extract raster values for bg points
 fus_bgvals <- values(fus_bg_vec) # Extract raster values for bg points
@@ -256,53 +253,53 @@ chl_sdmData <- data.frame(cbind(chl_pb, rbind(chl_predvals, cor_bgvals)))
 
 #Save df for downstream SDM work
 
-saveRDS(cor_sdmData, file = './occ_data/cor/cor_sdmData.Rdata')
-saveRDS(fus_sdmData, file = './occ_data/fus/fus_sdmData.Rdata')
-saveRDS(ion_sdmData, file = './occ_data/ion/ion_sdmData.Rdata')
-saveRDS(ang_sdmData, file = './occ_data/ang/ang_sdmData.Rdata')
-saveRDS(chl_sdmData, file = './occ_data/chl/chl_sdmData.Rdata')
+## saveRDS(cor_sdmData, file = './occ_data/cor/cor_sdmData.Rdata')
+## saveRDS(fus_sdmData, file = './occ_data/fus/fus_sdmData.Rdata')
+## saveRDS(ion_sdmData, file = './occ_data/ion/ion_sdmData.Rdata')
+## saveRDS(ang_sdmData, file = './occ_data/ang/ang_sdmData.Rdata')
+## saveRDS(chl_sdmData, file = './occ_data/chl/chl_sdmData.Rdata')
 
-cor_sdmData <- readRDS(file = 'cor_sdmData.Rdata')
+## cor_sdmData <- readRDS(file = 'cor_sdmData.Rdata')
 
 # Check for colinearity of predictor variables for presence-bg -----------
 # Although Maxent is equipped to handle variable selection through its built in variable regularization,
 # it is still important to under the relationships between predictors.
 # Dendograms useful for identifying groupings or clusters of colinear variables
 
-# M. coronaria
-pairs(cor_sdmData[,-1]) # drop the first column of 0/1
+## # M. coronaria
+## pairs(cor_sdmData[,-1]) # drop the first column of 0/1
 
-# Dendogram cluster of predictor colinearity
-threshold <- 0.7 # set the threshold for colinearity 
-cor_cors <- raster.cor.matrix(wclim_cor) # pearson correlation
-cor_dist <- as.dist(1 - abs(cor_cors)) # calculate distance of predictors
+## # Dendogram cluster of predictor colinearity
+## threshold <- 0.7 # set the threshold for colinearity 
+## cor_cors <- raster.cor.matrix(wclim_cor) # pearson correlation
+## cor_dist <- as.dist(1 - abs(cor_cors)) # calculate distance of predictors
 
-cor_clust <- hclust(cor_dist, method = 'single') # calculate cluster dendogram
-cor_groups <- cutree(cor_clust, h = 1 - threshold) #calculate groupings of variables
+## cor_clust <- hclust(cor_dist, method = 'single') # calculate cluster dendogram
+## cor_groups <- cutree(cor_clust, h = 1 - threshold) #calculate groupings of variables
 
-plot(cor_clust, hang = -1, main = "M. coronaria Predictors")
-rect.hclust(cor_clust, h = 1 - threshold)
+## plot(cor_clust, hang = -1, main = "M. coronaria Predictors")
+## rect.hclust(cor_clust, h = 1 - threshold)
 
 
-#M. fusca
-pairs(fus_sdmData[,2:5])
-pairs(fus_sdmData[,6:9])
-pairs(fus_sdmData[,10:13])
-pairs(fus_sdmData[,14:17])
-pairs(fus_sdmData[,17:20])
+## #M. fusca
+## pairs(fus_sdmData[,2:5])
+## pairs(fus_sdmData[,6:9])
+## pairs(fus_sdmData[,10:13])
+## pairs(fus_sdmData[,14:17])
+## pairs(fus_sdmData[,17:20])
 
-pairs(fus_sdmData[,-1])
+## pairs(fus_sdmData[,-1])
 
-# Dendogram cluster of predictor colinearity
-threshold <- 0.7 # set the threshold for colinearity 
-fus_cors <- raster.cor.matrix(wclim_fus)
-fus_dist <- as.dist(1 - abs(fus_cors)) # calculate distance of predictors
+## # Dendogram cluster of predictor colinearity
+## threshold <- 0.7 # set the threshold for colinearity 
+## fus_cors <- raster.cor.matrix(wclim_fus)
+## fus_dist <- as.dist(1 - abs(fus_cors)) # calculate distance of predictors
 
-fus_clust <- hclust(fus_dist, method = 'single') # calculate cluster dendogram
-fus_groups <- cutree(fus_clust, h = 1 - threshold) #calculate groupings of variables
+## fus_clust <- hclust(fus_dist, method = 'single') # calculate cluster dendogram
+## fus_groups <- cutree(fus_clust, h = 1 - threshold) #calculate groupings of variables
 
-plot(fus_clust, hang = -1, main = "M. fusca Predictors")
-rect.hclust(fus_clust, h = 1 - threshold)
+## plot(fus_clust, hang = -1, main = "M. fusca Predictors")
+## rect.hclust(fus_clust, h = 1 - threshold)
 
 
 
@@ -311,54 +308,54 @@ rect.hclust(fus_clust, h = 1 - threshold)
 # Presence points and background points
 
 
-cor_occ.temp <- cor_sdmData %>% filter(cor_pb == 1) %>% # Presence points
-  dplyr::select(wc2.1_2.5m_bio_1) %>% # Mean annual temp
-  drop_na() %>% 
-  unlist()
-cor_bg.temp <- cor_sdmData %>% filter(cor_pb == 0) %>% # Background points
-  dplyr::select(wc2.1_2.5m_bio_1) %>% # Mean annual temp
-  drop_na() %>% 
-  unlist()
+## cor_occ.temp <- cor_sdmData %>% filter(cor_pb == 1) %>% # Presence points
+##   dplyr::select(wc2.1_2.5m_bio_1) %>% # Mean annual temp
+##   drop_na() %>% 
+##   unlist()
+## cor_bg.temp <- cor_sdmData %>% filter(cor_pb == 0) %>% # Background points
+##   dplyr::select(wc2.1_2.5m_bio_1) %>% # Mean annual temp
+##   drop_na() %>% 
+##   unlist()
 
-cor_occ.perc <- cor_sdmData %>% filter(cor_pb == 1) %>% # Presence points
-  dplyr::select(wc2.1_2.5m_bio_12) %>% # Annual precipitation
-  drop_na() %>% 
-  unlist()
+## cor_occ.perc <- cor_sdmData %>% filter(cor_pb == 1) %>% # Presence points
+##   dplyr::select(wc2.1_2.5m_bio_12) %>% # Annual precipitation
+##   drop_na() %>% 
+##   unlist()
 
-cor_bg.perc <- cor_sdmData %>% filter(cor_pb == 0) %>% # Background points
-  dplyr::select(wc2.1_2.5m_bio_12) %>% # Annual precipitation
-  drop_na() %>% 
-  unlist()
-
-
-cor_occ.3d <- kde2d(cor_occ.temp, cor_occ.perc)
-cor_bg.3d <- kde2d(cor_bg.temp, cor_bg.perc)
-
-#Plot 3D surface Kernel density estimation
-
-plot_cor.occ_3d <- plot_ly(x=cor_occ.3d$x, y=cor_occ.3d$y, z=cor_occ.3d$z) %>% 
-  add_surface() %>% 
-  layout(scene = list(xaxis = list(title = 'Mean Annual Temp (C)', autotick = F, nticks = 5, tickvals = list(0,5,10,15,20)), 
-                      yaxis = list(title = 'Mean Annual Percip. (mm)', tick0=0, tick1=2000, dtick=200), 
-                      zaxis = list(title = 'Kernel Density', tick0=0, tick1=0.001, dtick=0.0002)),
-         title = list(text = "<i>M. coronaria<i> Occurrence Points", 
-                      y = 0.95, x = 0.5, 
-                      xanchor = 'center', 
-                      yanchor = 'top'))
-
-plot_cor.occ_3d # run to view
-
-plot_cor.bg_3d <- plot_ly(x=cor_bg.3d$x, y=cor_bg.3d$y, z=cor_bg.3d$z) %>% 
-  add_surface() %>% 
-  layout(scene = list(xaxis = list(title = 'Mean Annual Temp (C)', tick0=0, tick1=20, dtick=5), 
-                      yaxis = list(title = 'Mean Annual Percip. (mm)', tick0=0, tick1=2000, dtick=200), 
-                      zaxis = list(title = 'Kernel Density')),
-         title = list(text = "<i>M. coronaria<i> Background Points", 
-                      y = 0.95, x = 0.5, 
-                      xanchor = 'center', 
-                      yanchor = 'top'))
-
-plot_cor.bg_3d
+## cor_bg.perc <- cor_sdmData %>% filter(cor_pb == 0) %>% # Background points
+##   dplyr::select(wc2.1_2.5m_bio_12) %>% # Annual precipitation
+##   drop_na() %>% 
+##   unlist()
 
 
+## cor_occ.3d <- kde2d(cor_occ.temp, cor_occ.perc)
+## cor_bg.3d <- kde2d(cor_bg.temp, cor_bg.perc)
+
+## #Plot 3D surface Kernel density estimation
+
+## plot_cor.occ_3d <- plot_ly(x=cor_occ.3d$x, y=cor_occ.3d$y, z=cor_occ.3d$z) %>% 
+##   add_surface() %>% 
+##   layout(scene = list(xaxis = list(title = 'Mean Annual Temp (C)', autotick = F, nticks = 5, tickvals = list(0,5,10,15,20)), 
+##                       yaxis = list(title = 'Mean Annual Percip. (mm)', tick0=0, tick1=2000, dtick=200), 
+##                       zaxis = list(title = 'Kernel Density', tick0=0, tick1=0.001, dtick=0.0002)),
+##          title = list(text = "<i>M. coronaria<i> Occurrence Points", 
+##                       y = 0.95, x = 0.5, 
+##                       xanchor = 'center', 
+##                       yanchor = 'top'))
+
+## plot_cor.occ_3d # run to view
+
+## plot_cor.bg_3d <- plot_ly(x=cor_bg.3d$x, y=cor_bg.3d$y, z=cor_bg.3d$z) %>% 
+##   add_surface() %>% 
+##   layout(scene = list(xaxis = list(title = 'Mean Annual Temp (C)', tick0=0, tick1=20, dtick=5), 
+##                       yaxis = list(title = 'Mean Annual Percip. (mm)', tick0=0, tick1=2000, dtick=200), 
+##                       zaxis = list(title = 'Kernel Density')),
+##          title = list(text = "<i>M. coronaria<i> Background Points", 
+##                       y = 0.95, x = 0.5, 
+##                       xanchor = 'center', 
+##                       yanchor = 'top'))
+
+## plot_cor.bg_3d
+
+message("** Background Data Calculated")
 
