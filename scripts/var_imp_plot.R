f41b0e0db57622a2fbddf8acb86385023da80666
TerrellRoulston <terrellroulston@hotmail.ca>
2025-05-09 14:12:28 -0300

    dropped global rank lables too confusing

--- scripts/var_imp_plot.R
+++ scripts/var_imp_plot.R
@@ -170,12 +170,6 @@ p_sp <- ggplot(plot_data_by_sp, aes(x = Importance, y = Variable_reordered, colo
                linetype = "dashed", linewidth = 1.25) +
   # points colored by Variable
   geom_point(aes(color = Variable), size = 5) +
-  # rank labels
-  #geom_text(aes(label = VarRank), nudge_x = 1, fontface = "bold", size = 5, color = "black") +
-  geom_text(aes(x = 0, label = VarRank),
-            hjust = 4, vjust = 0.5,
-            fontface = "bold", size = 5,
-            color = "black") +
   # facet by Species
   facet_wrap(~ Species, 
              ncol   = 1, 
