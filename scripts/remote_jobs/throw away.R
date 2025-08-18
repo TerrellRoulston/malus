46449b1ea702627dcd19ec1e2a8b1bc4bd6dd095
TerrellRoulston <terrellroulston@hotmail.ca>
2025-08-18 16:22:49 -0300

    deleting unneeded remote job scripts

--- scripts/remote_jobs/throw away.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-files <- list.files("./", pattern = "niche_equiv_.*\\.rds", full.names = TRUE)
-
-results <- map_dfr(files, readRDS, .id = "pair")
-results$pair <- gsub("niche_equiv_|\\.rds", "", basename(results$pair))
-
-print(results)
