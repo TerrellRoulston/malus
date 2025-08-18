52e7629b5bdbb9c58960640bed8e63c78d76af46
TerrellRoulston <terrellroulston@hotmail.ca>
2025-08-18 16:01:28 -0300

    Organized and added libs to the list Tyler started

--- scripts/libraries.R
+++ scripts/libraries.R
@@ -1,22 +1,30 @@
-library(MASS) ## loading first so the geospatial packages will take
-## precedence, hopefully?
-library(sf)
-library(rnaturalearth)
-library(tidyverse) #grammar, data management
-library(CoordinateCleaner) #helpful functions to clean data
-library(terra) #working with vector/raster data
-library(geodata) #download basemaps
-library(scales) #alpha adjust colours
-library(oce)
-library(predicts)
-library(ENMTools)
-library(plotly) # 3D surface Kernel bivariate plots
-library(ENMeval) # Another modeling package, useful for data partitioning (Checkerboarding)
-library(ecospat) # Useful spatial ecology tools
-library(parallel) # speed up computation by running in parallel
-library(doParallel) # added functionality to parallel
-library(ade4)
-library(grid)
+# List of libraries required for Main Analysis 
+# Source this script in the analysis script to load all libs
+
+### Data Management and Grammer
+library(tidyverse) # Grammer and fundamental packages
+library(readr) # Loading text files
+
+### GIS Data
+library(terra) # GIS package, geospatial data managment, spatial analysis, plotting
+library(geodata) # Basemaps, and WorldClim Data (Historical and climate change projections) (NOTE: loading WorldClim data from memory via project)
+library(wdpar) # World Database for Protected Areas, PA data for gap analysis
+library(CoordinateCleaner) # Automated cleaning of occurrence data
 
 
+### SDM Dependacies
+library(rJava) # Dependacy for Maxent.jar via Java
+library(predicts) # Base SDM package, Runs via ENMeval
+library(ENMeval) # Load for additioanl functions for tuning SDMs
+library(parallel) # Speed up SDM computation by running in parallel
+library(doParallel) # Added functionality to parallel
+library(dismo) # For plotting response curves (accepts Maxent objects) -- another SDM package
 
+
+### Ecological Niche Modeling
+library(ade4) # Multivariate PCA niche analysis
+# Note, one function from the Ecospat package has a flaw.
+# Tyler Smith has updated this function to work correctly, install this fix via his github
+library(devtools) # Github remote access
+devtools::install_github("plantarum/ecospat", ref = "boyce", subdir = "ecospat")
+library(ecospat) # Ecolgical niche analysis
